5afede86fe1d83b6933f3d65c29dee045fa6bd39
Derick Rethans <github@derickrethans.nl>
2012-10-04 10:13:20 +0100

    Convert pull.php into a classy thing.

--- pull.php
+++ pull.php
@@ -1,33 +1,69 @@
 <?php
 include 'config.php';
 
-function fetch($call)
+class Github
 {
-	$base = "https://api.github.com/";
-	return json_decode(file_get_contents("$base$call"));
-}
+	static $base = "https://api.github.com/";
 
-foreach ($d->query("SELECT name FROM user") as $row)
-{
-	$name = $row['name'];
-	echo "Fetching for {$name}\n";
-
-	foreach (fetch("users/{$name}/repos") as $r) 
+	function call( $method )
 	{
-		if ($r->fork) {
-			continue;
-		}
-		echo $r->name, ': ', $r->description, "\n";
+		return json_decode(file_get_contents(self::$base . $method));
+	}
 
-		echo "- Branches:\n";
-		foreach (fetch("repos/{$name}/{$r->name}/branches") as $b) {
-			echo "  - ". $b->name, "\n";
+	private function storeRepository( $d, $name, $reponame, $branches, $languages )
+	{
+		$canonicalName = "$name/{$reponame}";
+
+		/* First we store the repository name */
+		$d->query( "INSERT INTO repo VALUES('$canonicalName', 0)" );
+
+		/* Then languages and branches */
+		foreach ( $branches as $branch )
+		{
+			$d->query( "INSERT INTO branch VALUES('$canonicalName', '$branch');" );
+		}
+		foreach ( $languages as $lang )
+		{
+			$d->query( "INSERT INTO lang VALUES('$canonicalName', '$lang');" );
 		}
 
-		echo "- Languages:\n";
-		foreach (fetch("repos/{name}/{$r->name}/languages") as $l => $count) {
-			echo "  - $l; bytes: $count\n";
+	}
+
+	private function processUser( $d, $name )
+	{
+		foreach ( $this->call("users/{$name}/repos") as $r )
+		{
+			if ( $r->fork ) {
+				continue;
+			}
+
+			$branches = $languages = array();
+
+			foreach ( $this->call( "repos/{$name}/{$r->name}/branches" ) as $b )
+			{
+				$branches[] = $b->name;
+			}
+
+			foreach ( $this->call( "repos/{$name}/{$r->name}/languages" ) as $l => $count )
+			{
+				$languages[] = $l;
+			}
+
+			$this->storeRepository( $d, $name, $r->name, $branches, $languages );
 		}
-		echo "\n";
+	}
+
+	function processUsers( $d )
+	{
+		foreach ( $d->query("SELECT name FROM user") as $row )
+		{
+			$name = $row['name'];
+			$this->processUser( $d, $name );
+		}
 	}
 }
+
+$g = new Github;
+$g->processUsers( $d );
+echo "Done!\n";
+?>

b1ff2387f0d5c25b0ae2d4c6f777f7f372e49fd7
Derick Rethans <github@derickrethans.nl>
2012-10-04 10:12:38 +0100

    Fixed setup script.

--- setup.php
+++ setup.php
@@ -4,5 +4,5 @@ include 'config.php';
 @$d->query("CREATE TABLE user(name char, fullname char)");
 @$d->query("CREATE TABLE repo(name char, fork int)");
 @$d->query("CREATE TABLE branch(repo char, branch char)");
-@$d->query("CREATE TABLE lang(char repo, char lang)");
+@$d->query("CREATE TABLE lang(repo char, lang char)");
 ?>
